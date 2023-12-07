7f69330e11d0bb040594aaeb0d37add69efa1568
Diogo Canut <diogocanut@hotmail.com>
2023-12-07 00:05:58 -0300

    running health check tests on a random port

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let port = 8000;
+    let listener = TcpListener::bind(&format!("127.0.0.1:{}", port))
+        .expect(&format!("Failed to bind port {}", port));
+    run(listener)?.await
 }

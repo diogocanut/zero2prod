e5301840c497342c2b6381b66a5dd9cff9ceb9d5
Diogo Canut <diogocanut@hotmail.com>
2023-12-07 00:09:53 -0300

    fix: use unwrap_or_else isntead of expect

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use zero2prod::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let port = 8000;
-    let listener = TcpListener::bind(&format!("127.0.0.1:{}", port))
-        .expect(&format!("Failed to bind port {}", port));
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", port))
+        .unwrap_or_else(|_| panic!("Failed to bind port {}", port));
     run(listener)?.await
 }

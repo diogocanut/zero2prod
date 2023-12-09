79e2ff38d807a1fed97dfb5233236fb07c65dd8c
Diogo Canut <diogocanut@hotmail.com>
2023-12-08 21:11:55 -0300

    reorganize file structure and read configuration file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::net::TcpListener;
-use zero2prod::run;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let port = 8000;
-    let listener = TcpListener::bind(format!("127.0.0.1:{}", port))
-        .unwrap_or_else(|_| panic!("Failed to bind port {}", port));
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
